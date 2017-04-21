a40060ebe20f001b333d4e1c7afb4c19f18b61b8
Cheshirilik <avfriend@mail.ru>
2017-04-21 22:10:58 +0300

    Main game loop

--- arnd.py
+++ arnd.py
@@ -1,11 +1,24 @@
+import tkinter as tk
 
 
-from Tkinter import Tk
+class Game(tk.Frame):
 
-Main = Tk()
+    def __init__(self, master):
+        super(Game, self).__init__(master)
 
-Main.title("Main window")
-Main.mainloop()
+        self.lives = 3
+        self.width = 800
+        self.height = 600
 
-print ("hello")
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height, bg='#aaaaff' )
+        self.canvas.pack()
+        self.pack()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Arknd")
+
+    game = Game(root)
+    game.mainloop()
 
