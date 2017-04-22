afd830a832aa912f87059e66b81406a6c5be2f96
Cheshirilik <avfriend@mail.ru>
2017-04-22 21:04:09 +0300

    Ball and Paddle classes added

--- arnd.py
+++ arnd.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+import time
 
-
+# Abstract game object
 class GameObject(object):
     def __init__(self, canvas, item):
         self.canvas = canvas
@@ -15,7 +16,35 @@ class GameObject(object):
     def delete(self, item):
         self.canvas.delete(self.item)
 
+# Ball. Move only diagonally.
+class TheBall(GameObject):
+    def __init__(self, canvas, x, y):
+        self.radius = 10
+        self.direction = [-1, 1]
+        self.speed = 10
 
+        item = canvas.create_oval(x-self.radius, y-self.radius,
+                                  x+self.radius, y+self.radius,
+                                  fill="white")
+
+        super(TheBall, self).__init__(canvas, item)
+
+
+# Paddle. Move only horizontally
+class ThePaddle(GameObject):
+    def __init__(self, canvas, x, y):
+        self.width = 80
+        self.height = 10
+        self.ball = None
+
+        item = canvas.create_rectangle(x-self.width/2, y-self.height/2,
+                                  x+self.width/2, y+self.height/2,
+                                  fill="blue")
+
+        super(ThePaddle, self).__init__(canvas, item)
+
+
+# main loop.
 class Game(tk.Frame):
 
     def __init__(self, master):
@@ -26,13 +55,23 @@ class Game(tk.Frame):
         self.height = 600
 
         self.canvas = tk.Canvas(self, width=self.width, height=self.height, bg='#aaaaff' )
+
+        self.item = self.canvas.create_rectangle(10, 10, 100, 80, fill='green')
+
+        #time.sleep(5)
+        obj = GameObject(self.canvas, self.item)
+        obj.move(50, 50)
+
         self.canvas.pack()
         self.pack()
+
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Arknd")
+
+
 
     game = Game(root)
     game.mainloop()
