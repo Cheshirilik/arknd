b26c77f3cf07e84a854cb33ca29171bf8bfd5ab7
Cheshirilik <avfriend@mail.ru>
2017-04-26 17:36:25 +0300

    Game class added

--- arnd.py
+++ arnd.py
@@ -54,6 +54,7 @@ class ThePaddle(GameObject):
             if self.ball is not None:
                 self.ball.move(offset, 0)
 
+
 # Brick
 class TheBrick(GameObject):
 
@@ -63,7 +64,7 @@ class TheBrick(GameObject):
         self.width = 75
         self.height = 20
         self.hits = hits
-        color = Brick.COLORS[hits]
+        color = TheBrick.COLORS[hits]
         item = canvas.create_rectangle(x-self.width/2, y-self.height/2,
                                   x+self.width/2, y+self.height/2,
                                   fill=color, tag="brick")
@@ -78,35 +79,79 @@ class TheBrick(GameObject):
         else:
             self.canvas.itemconfig(self.item, fill=TheBrick.COLORS[self.hits])
 
+
 # main loop.
 class Game(tk.Frame):
+
+    # add ball in game
+    def add_ball(self):
+        if self.ball is not None:
+            self.ball.delete()
+
+        paddle_coords = self.paddle.get_position()
+        x = (paddle_coords[0] + paddle_coords[2]) * 0.5
+        self.ball = TheBall(self.canvas, x, 310)
+        self.paddle.set_ball(self.ball)
+
+    # add brick in game
+    def add_brick(self, x, y, hits):
+        brick = TheBrick(self.canvas, x, y, hits)
+        self.items[brick.item] = brick
+
+    # write text
+    def draw_text(self, x, y, text, size = "40" ):
+        font = ("Helvetica", size)
+        return self.canvas.create_text(x, y, text=text, font=font)
+
+    # update lives on screen
+    def update_lives_text(self):
+        text = "Lives: %s" % self.lives
+        if self.hud is None:
+            self.hud = self.draw_text(50, 20, text, 15)
+        else:
+            self.canvas.itemconfig(self.hud, text=text)
+
+    def start_game(self):
+        pass
+
+    def setup_game(self):
+        self.add_ball()
+        self.update_lives_text()
+        self.text = self.draw_text(300, 200, "Press <Space> to start")
+
+        self.canvas.bind("<space>", lambda _: self.start_game())
 
     def __init__(self, master):
         super(Game, self).__init__(master)
 
         self.lives = 3
-        self.width = 800
-        self.height = 600
+        self.width = 610
+        self.height = 400
 
-        self.canvas = tk.Canvas(self, width=self.width, height=self.height, bg='#aaaaff' )
-
-        self.item = self.canvas.create_rectangle(10, 10, 100, 80, fill='green')
-
-        #time.sleep(5)
-        obj = GameObject(self.canvas, self.item)
-        obj.move(50, 50)
-
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height, bg='#aaaaff')
         self.canvas.pack()
         self.pack()
 
+        self.items = {}
+        self.ball = None
+        self.paddle = ThePaddle(self.canvas, self.width/2, 326)
+        self.items[self.paddle.item] = self.paddle
+
+        for i in range(5, self.width - 5, 75):
+            self.add_brick(i + 37.5, 50, 3)
+            self.add_brick(i + 37.5, 70, 2)
+            self.add_brick(i + 37.5, 90, 1)
+
+        self.hud = None
+        self.setup_game()
+        self.canvas.focus_set()
+        self.canvas.bind("<Left>", lambda _ : self.paddle.move(-10))
+        self.canvas.bind("<Right>", lambda _: self.paddle.move(10))
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Arknd")
-
-
-
     game = Game(root)
     game.mainloop()
 
