2eaa6df7bcbe58348db4a4ba2a7f3ec880f0e407
Cheshirilik <avfriend@mail.ru>
2017-04-26 16:39:29 +0300

    Brick class added

--- arnd.py
+++ arnd.py
@@ -54,7 +54,29 @@ class ThePaddle(GameObject):
             if self.ball is not None:
                 self.ball.move(offset, 0)
 
+# Brick
+class TheBrick(GameObject):
 
+    COLORS = {1:"#999999", 2:"#555555", 3:"#222222"}
+
+    def __init__(self, canvas, x, y, hits):
+        self.width = 75
+        self.height = 20
+        self.hits = hits
+        color = Brick.COLORS[hits]
+        item = canvas.create_rectangle(x-self.width/2, y-self.height/2,
+                                  x+self.width/2, y+self.height/2,
+                                  fill=color, tag="brick")
+
+        super(TheBrick, self).__init__(canvas, item)
+
+
+    def hit(self):
+        self.hits -= 1
+        if self.hits == 0:
+            self.delete()
+        else:
+            self.canvas.itemconfig(self.item, fill=TheBrick.COLORS[self.hits])
 
 # main loop.
 class Game(tk.Frame):
